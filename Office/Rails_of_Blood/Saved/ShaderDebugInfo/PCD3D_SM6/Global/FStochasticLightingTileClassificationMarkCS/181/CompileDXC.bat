6851010e5668edd3276fc54ce00b2882a877155f
TheSnazzyMaster <rubystott06@outlook.com>
2026-02-11 01:09:47 +0000

    File Removal

--- Office/Rails_of_Blood/Saved/ShaderDebugInfo/PCD3D_SM6/Global/FStochasticLightingTileClassificationMarkCS/181/CompileDXC.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SET DXC="C:\Program Files\Epic Games\UE_5.7\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
-IF NOT EXIST %DXC% (
-	ECHO Couldn't find dxc.exe under "C:\Program Files\Epic Games\UE_5.7\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
-	GOTO :END
-)
-%DXC% -HV 2021 -Zpr -O3 -auto-binding-space 0 -Wno-parentheses-equality -disable-lifetime-markers -T cs_6_6 -E StochasticLightingTileClassificationMarkCS -Fc StochasticLightingTileClassification.d3dasm -Fo StochasticLightingTileClassification.dxil StochasticLightingTileClassification.usf
-:END
-PAUSE
